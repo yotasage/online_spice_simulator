3e941e8244a48dee118b02cdcc6ef19e04118fc5
Jonathan Sæthre Ege <jonathan.s.ege@ntnu.no>
2024-09-16 23:10:52 +0200

    CLEAN-UP

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,16 +3,8 @@
 import Image from 'next/image'
 import "../app/globals.css";
 import { useState, useEffect } from 'react'
-//import { Readable } from 'node:stream';
 
-// const getData = async() => {
-//   console.log("data from server")
-//   const response = await get('/api/ngspice')
-//   return response?.data
-// }
-
-const getDataa = async() => {
-  console.log("data from server")
+const getSimOutput = async() => {
   const res = await fetch('/api/ex')
   const res_json = res.json()
   return res_json
@@ -47,17 +39,17 @@ const getNetlist = async() => {
 export default function About() {
 
     const handleClick = async() => {
-      const endpointDataa = await getDataa()
-      console.log(endpointDataa)
-      setDataa(endpointDataa)
+      const endpointsimOutput = await getSimOutput()
+      console.log(endpointsimOutput)
+      setSimOutput(endpointsimOutput)
 
       const endpointData = await getData()
       console.log(JSON.parse(endpointData)["i(vload)"]["data"][20])
     }
 
     //const staticData = await fetch('/api/ngspice', { cache: 'force-cache' })
-    const [data, setData] = useState('null')
-    const [dataa, setDataa] = useState('null')
+    const [simVersion, setSimVersion] = useState('null')
+    const [simOutput, setSimOutput] = useState('null')
     const [netlist, setNetlist] = useState('null')
     const [isLoading, setLoading] = useState(true)
 
@@ -65,7 +57,7 @@ export default function About() {
         fetch('/api/ngspice')
           .then((res) => res.json())
           .then((data) => {
-            setData(data)
+            setSimVersion(data)
             setLoading(false)
           })
     }, [])
@@ -79,16 +71,15 @@ export default function About() {
     }, [])
      
     if (isLoading) return <p>Loading...</p>
-    if (!data) return <p>No profile data</p>
-    if (!dataa) return <p>No profile data</p>
-    if (!netlist) return <p>No profile data</p>
+    if (!simVersion) return <p>Loading simulator version</p>
+    if (!simOutput) return <p>No profile data</p>
+    if (!netlist) return <p>Loading netlist</p>
       
 
     return (
     <div>
-      <p>circuit</p>
-      <button onClick={handleClick}>Run</button>
-      <div className="border-double border-4 border-sky-500  my-5 w-2/4">
+      
+      <div className="border-double border-4 border-sky-500 top-20 left-0 bottom-1/2 absolute inline-block w-2/4">
         <Image
         priority
         src="./examples/acmos1/nmos_current_mirror/fig.svg"
@@ -97,18 +88,22 @@ export default function About() {
         height={500}
         alt="Example circuit"/>
       </div>
-      <p>ngspice version information</p>
-      <div className="border-double border-4 border-sky-500  my-5 w-2/4">
-        <p className="whitespace-pre">{data.message}</p>
+      
+      <div className="border-double border-4 border-sky-500 top-0 right-0 absolute inline-block w-2/4 h-1/6">
+        <p className="whitespace-pre">{simVersion.message}</p>
       </div>
 
-      <div className="border-double border-4 border-sky-500  my-5 w-2/4">
-        <p className="whitespace-pre">{dataa.message}</p>
+      <div className="border-double border-4 border-sky-500 bottom-0 left-0 absolute inline-block w-2/4 h-2/4">
+        <p className="whitespace-pre">{simOutput.message}</p>
       </div>
 
-      <div className="border-double border-4 border-sky-500  my-5 w-2/4">
+      <div className="border-double border-4 border-sky-500 bottom-0 right-0 absolute inline-block w-2/4 h-5/6">
         <p className="whitespace-pre">{netlist}</p>
       </div>
+
+      <p>ngspice version information</p>
+      <p>circuit</p>
+      <button onClick={handleClick}>Run</button>
     </div>
     
     )
--- src/pages/api/netlist.ts
+++ src/pages/api/netlist.ts
@@ -1,35 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-// import { promises as fs } from 'fs';
-//import 'fs';
 const fs  = require("fs");
-
-const util = require('node:util');
-const exec = util.promisify(require('node:child_process').exec);
-
-type ResponseData = {
-  message: string
-}
   
 export default async function netlist(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse
 ) {
-    //const { stdout, stderr } = await exec('cat local_assets\\examples\\acmos1\\nmos_current_mirror\\dc.spi');
-    //res.status(200).json({ message: stdout })
-
     var filePath = 'local_assets\\examples\\acmos1\\nmos_current_mirror\\dc.spi'
 
-    //const file = await fs.readFile(filePath, 'utf8');
     var file = fs.createReadStream(filePath, { encoding: 'utf8' });
     var stat = fs.statSync(filePath);
-    //var stat = fs.stat(filePath);
 
-    //console.log(stat.size)
-
-    //res.setHeader('Content-Length', (await stat).size);
     res.setHeader('Content-Length', stat.size);
     res.setHeader('Content-Type', 'text/html');
-    //res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
 
     // TODO: This might not be neccesary as it seems like this was intended for a case when writing to the file.
     // Add this to ensure that the out.txt's file descriptor is closed in case of error.
@@ -37,7 +19,5 @@ export default async function netlist(
         file.end();
     });
 
-    // res.status(200).json({ message: file })
     file.pipe(res);
-    
 }
